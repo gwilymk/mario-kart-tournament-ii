0a3b93d497e5370d118a033e406d7f9cdfc9053b
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-08-27 22:18:17 +0100

    Add a `useLocalStorage` variant for immer

--- src/lib/hooks/useLocalStorage.ts
+++ src/lib/hooks/useLocalStorage.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import { useImmer } from "use-immer";
 
-export default function useLocalStorage<T>(key: string, initialValue: T) {
+export const useLocalStorageState = <T>(key: string, initialValue: T) => {
     const [value, setValue] = useState<T>(initialValue);
 
     // Handle loading the initial value
@@ -15,4 +16,21 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
     }, [key, value]);
 
     return [value, setValue] as const;
-}
+};
+
+export const useLocalStorageImmer = <T>(key: string, initialValue: T) => {
+    const [value, setValue] = useImmer<T>(initialValue);
+
+    // Handle loading the initial value
+    useEffect(() => {
+        const stored = localStorage.getItem(key);
+        setValue(stored !== null ? JSON.parse(stored) : initialValue);
+    }, [key, initialValue, setValue]);
+
+    // Handle when the value changes
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue] as const;
+};
