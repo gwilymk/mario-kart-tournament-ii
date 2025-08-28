c02e40e8d9bd984a90a17b7a897d9ecde56aad37
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-08-28 22:54:30 +0100

    Allow changing the group size

--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -1,6 +1,10 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
+import { IconButton } from "@/components/Button";
 import { GroupPlayer } from "@/components/Group/GroupPlayer";
+import { useTournament } from "@/components/TournamentContext";
 import { Group as PlayerGroup } from "@/lib/tournament";
 import css from "./Group.module.css";
 
@@ -11,6 +15,8 @@ interface GroupProps {
 }
 
 export const Group: FC<GroupProps> = ({ group, number, showPlacement }) => {
+    const { updateGroupSize } = useTournament();
+
     const groupIndex = (number % 5) + 1;
 
     return (
@@ -21,7 +27,16 @@ export const Group: FC<GroupProps> = ({ group, number, showPlacement }) => {
                 borderColor: `var(--group-${groupIndex}-color-light)`,
             }}
         >
-            <h2 className={css.title}>Group {number + 1}</h2>
+            <div className={css.header}>
+                <IconButton type="button" onClick={() => updateGroupSize(number, "down")} className={css.icon}>
+                    <RemoveCircleOutlineIcon />
+                </IconButton>
+                <h2 className={css.title}>Group {number + 1}</h2>
+
+                <IconButton type="button" onClick={() => updateGroupSize(number, "up")} className={css.icon}>
+                    <AddCircleOutlineIcon />
+                </IconButton>
+            </div>
             <ol className={css.players}>
                 {group.players.map((player) => (
                     <GroupPlayer player={player} key={player.id} showPlacement={showPlacement} />
