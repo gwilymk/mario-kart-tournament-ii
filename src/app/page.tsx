c622cdf7ee1133ca0959bbf48a9cb44a0d56771f
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-08-30 15:29:36 +0100

    Add a new homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,33 @@
 "use client";
 
-import { useReducer, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-import TournamentDisplay from "@/components/TournamentDisplay";
-import { Tournament } from "@/lib/tournament";
-import styles from "./page.module.css";
+import css from "@/app/page.module.css";
+import { PrimaryButton } from "@/components/Button";
 
-const tournament = new Tournament();
-
-export default function Home(): React.ReactNode {
-    const [_, forceUpdate] = useReducer((x) => x + 1, 0);
+export default function Home() {
+    const router = useRouter();
 
     return (
-        <div className={styles.page}>
-            <main className={styles.main}>
-                <AddPlayer
-                    onAdd={(name) => {
-                        tournament.addPlayer(name);
-                        forceUpdate();
-                    }}
-                />
-                <TournamentDisplay
-                    groups={tournament.getGroups()}
-                    movePlayer={(id, direction) => {
-                        tournament.movePlayer(id, direction);
-                        forceUpdate();
-                    }}
-                    updateGroupSize={(id, direction) => {
-                        tournament.updateGroupSize(id, direction);
-                        forceUpdate();
-                    }}
-                    completeRound={() => {
-                        tournament.completeRound();
-                        forceUpdate();
-                    }}
-                />
+        <div className={css.page}>
+            <main className={css.main}>
+                <div className={css.logo}>
+                    <Image
+                        src="/images/mariokart-logo.svg"
+                        alt="Mariokart Logo"
+                        priority={true}
+                        fill={true}
+                        objectFit="contain"
+                        objectPosition="center"
+                    />
+                </div>
+                <div className={css.controls}>
+                    <PrimaryButton type="button" onClick={() => router.push("/tournament/player-setup")}>
+                        Create tournament
+                    </PrimaryButton>
+                </div>
             </main>
         </div>
-    );
-}
-
-function AddPlayer({ onAdd }: Readonly<{ onAdd: (name: string) => void }>): React.ReactNode {
-    const [name, setName] = useState("");
-
-    return (
-        <label>
-            <span>Add player:</span>
-            <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key == "Enter") {
-                        onAdd(name);
-                        setName("");
-                    }
-                }}
-            />
-        </label>
     );
 }
