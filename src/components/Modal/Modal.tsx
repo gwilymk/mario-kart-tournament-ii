569c52495d2c16a87964d8c5bcfe85c7b61911d8
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-08-30 11:00:03 +0100

    Add a modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { FC, PropsWithChildren, useEffect, useRef } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { IconButton } from "@/components/Button";
+import css from "./Modal.module.css";
+
+interface ModalProps extends PropsWithChildren {
+    isOpen: boolean;
+    onClose: () => void;
+    showCloseButton?: boolean;
+}
+
+export const Modal: FC<ModalProps> = ({ isOpen, onClose, showCloseButton = true, children }) => {
+    const modalRef = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+        const modalElement = modalRef.current;
+
+        if (!modalElement) {
+            return;
+        }
+
+        if (isOpen) {
+            modalElement.showModal();
+        } else {
+            modalElement.close();
+        }
+    }, [isOpen]);
+
+    return (
+        <div className={css.wrapper}>
+            <dialog ref={modalRef} className={css.modal} onClose={() => onClose()} closedby="any">
+                {showCloseButton && (
+                    <IconButton type="button" onClick={() => onClose()} className={css.close}>
+                        <CloseIcon />
+                    </IconButton>
+                )}
+                {children}
+            </dialog>
+        </div>
+    );
+};
